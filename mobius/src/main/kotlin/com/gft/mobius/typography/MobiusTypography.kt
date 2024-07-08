cd3315c41a36a26717320d0b352ca8f9b0fbf2ae
Krzysztof Lopacki <krzysztof.lopacki@gft.com>
2024-07-08 16:28:17 +0200

    Define Mobius typography scheme

---
+++ mobius/src/main/kotlin/com/gft/mobius/typography/MobiusTypography.kt
@@ -0,0 +1,27 @@
+package com.gft.mobius.typography
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.text.TextStyle
+
+@Immutable
+interface MobiusTypography {
+    val displayLarge: TextStyle
+    val displayMedium: TextStyle
+    val displaySmall: TextStyle
+
+    val headlineLarge: TextStyle
+    val headlineMedium: TextStyle
+    val headlineSmall: TextStyle
+
+    val titleLarge: TextStyle
+    val titleMedium: TextStyle
+    val titleSmall: TextStyle
+
+    val labelLarge: TextStyle
+    val labelMedium: TextStyle
+    val labelSmall: TextStyle
+
+    val bodyLarge: TextStyle
+    val bodyMedium: TextStyle
+    val bodySmall: TextStyle
+}
