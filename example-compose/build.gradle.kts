42fa8f8802fb9eb7b27d856b5560240ad1c474b2
Szymon N <e-snni@gft.com>
2024-11-20 10:14:51 +0100

    [Maintenance] Lower minSdk level to API 26

--- example-compose/build.gradle.kts
+++ example-compose/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.gft.designsystem"
-        minSdk = 29
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
